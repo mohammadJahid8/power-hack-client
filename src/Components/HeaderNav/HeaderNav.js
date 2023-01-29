80f84336159661c09fbd72bb79d7917a40882fce
Mohammad Jahid <mohammadjahid0007@gmail.com>
2023-01-30 01:20:11 +0600

    handled navbar

--- src/Components/HeaderNav/HeaderNav.js
+++ src/Components/HeaderNav/HeaderNav.js
@@ -1,9 +1,25 @@
+import { useContext } from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
+import { useNavigate } from "react-router-dom";
+import swal from "sweetalert";
+import { PowerHackUserContext } from "../../context/PowerHackUserContext";
 
 function HeaderNav() {
+  const { user, logout } = useContext(PowerHackUserContext);
+  const navigate = useNavigate();
+
+  const handleLogOut = () => {
+    logout();
+    return swal({
+      text: "Sign-up Successful!",
+      icon: "success",
+      button: "OK!",
+    });
+  };
+
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
@@ -12,7 +28,15 @@ function HeaderNav() {
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto"></Nav>
           <Nav>
-            <Nav.Link href="#deets">Logout</Nav.Link>
+            {user ? (
+              <Nav.Link href="#deets" onClick={handleLogOut}>
+                Sign Out
+              </Nav.Link>
+            ) : (
+              <Nav.Link href="#deets" onClick={() => navigate("/signin")}>
+                Sign In
+              </Nav.Link>
+            )}
           </Nav>
         </Navbar.Collapse>
       </Container>
